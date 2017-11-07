ac445942a17ba8d93fbd0a8bd6a03a68d907ef85
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2017-11-08 00:58:16 +0300

    more resilience

--- main.rb
+++ main.rb
@@ -5,12 +5,12 @@ require 'ruby-progressbar'
 
 require_relative 'api_wrapper'
 
-wrapper = ApiWrapper.new
+@wrapper = ApiWrapper.new
 
 def print_ps_banner(data)
-  ps_cost = 100_000_000_000
+  ps_cost    = 100_000_000_000
   time_to_ps = ((ps_cost - data.points) / data.pointsPerSecond).round
-  from_now = (Time.now + time_to_ps).strftime('%H:%M:%S')
+  from_now   = (Time.now + time_to_ps).strftime('%H:%M:%S')
   puts ('-' * 80).yellowish
   puts '|'.yellowish +
          "Time until can buy PS: #{time_to_ps.round} secs (or #{secs_to_time(time_to_ps)} (tomorrow at #{from_now}))".center(78) +
@@ -29,7 +29,7 @@ rescue
 end
 
 def eta(unit, data)
-  cost = unit.cost.send('1')
+  cost         = unit.cost.send('1')
   have_already = data.points
   need_to_have = cost - have_already
   need_to_have / data.pointsPerSecond
@@ -41,44 +41,54 @@ end
 
 def secs_to_time(secs)
   hours, secs = secs.divmod(3600)
-  mins, secs = secs.divmod(60)
+  mins, secs  = secs.divmod(60)
   "%02d:%02d:%02d" % [hours, mins, secs]
 end
 
-loop do
-  data = wrapper.init.data
-
-  print_ps_banner(data)
-
-  units_by_profit = data.units.sort_by do |unit|
-    unit.costPerCoin = unit.cost.send('1').fdiv(unit.itemProduction).round
-  end
-
-  units_by_profit.each_with_index do |unit, idx|
-    time = eta(unit, data)
-
-    msg = if time < 0
-      "#{unit.type}: cost per coin (#{unit.costPerCoin}), can buy NOW"
-    else
-      "#{unit.type}: cost per coin (#{unit.costPerCoin}), can buy in #{time.round} seconds"
-    end
-
-    msg = case idx
-    when 0
-      msg.greenish
-    when 1, 2
-      msg.yellowish
-    else
-      msg
-    end
-    puts msg
-  end
-
-  best_unit = units_by_profit.first
+def maybe_buy_unit(best_unit, data)
   if can_buy_unit?(best_unit, data)
-    wrapper.build(best_unit.type)
-    puts "\aBought #{best_unit.type}!".redish
+    @wrapper.build(best_unit.type)
+    msg = "Bought #{best_unit.type}!"
+    puts msg.redish
+    `say #{msg}`
   end
+end
 
-  pretty_sleep(eta(best_unit, data))
+loop do
+  begin
+    data = @wrapper.init.data
+
+    print_ps_banner(data)
+
+    units_by_profit = data.units.sort_by do |unit|
+      unit.costPerCoin = unit.cost.send('1').fdiv(unit.itemProduction).round
+    end
+
+    units_by_profit.each_with_index do |unit, idx|
+      time = eta(unit, data)
+
+      msg = if time < 0
+        "#{unit.type}: cost per coin (#{unit.costPerCoin}), can buy NOW"
+      else
+        "#{unit.type}: cost per coin (#{unit.costPerCoin}), can buy in #{time.round} seconds"
+      end
+
+      msg = case idx
+      when 0
+        msg.greenish
+      when 1, 2
+        msg.yellowish
+      else
+        msg
+      end
+      puts msg
+    end
+
+    best_unit = units_by_profit.first
+
+    maybe_buy_unit(best_unit, data)
+    pretty_sleep(eta(best_unit, data))
+  rescue
+    retry
+  end
 end
